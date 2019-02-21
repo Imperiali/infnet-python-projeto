480dfd7925b7d2e719b08e4154e78541583b039a
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-02-21 08:36:53 -0300

    Corregido

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,8 @@
-import socket, time, pickle, psutil
+import pickle
+import psutil
+import socket
+import time
+
 
 # Função que imprime a lista formatada
 def formatar_cpu_mem(l):
@@ -7,7 +11,8 @@ def formatar_cpu_mem(l):
         texto = texto + '{:>8.2f}'.format(i)
     print(texto)
 
-#Função que formata processos
+
+# Função que formata processos
 def formatar_processos_titulo():
     titulo = '{:^7}'.format("PID")
     titulo = titulo + '{:^11}'.format("# Threads")
@@ -20,7 +25,8 @@ def formatar_processos_titulo():
     titulo = titulo + " Executável"
     print(titulo)
 
-#função que formata processos
+
+# função que formata processos
 def formatar_processos_texto(pid):
     try:
         p = psutil.Process(pid)
@@ -38,12 +44,15 @@ def formatar_processos_texto(pid):
         print(texto)
     except:
         pass
-#Função que formata Redes
+
+
+# Função que formata Redes
 def redes_formatada(l):
     titulo = '{:21}'.format("Ip")
     titulo = titulo + '{:27}'.format("Netmask")
     titulo = titulo + '{:27}'.format("MAC")
     print(titulo)
+
 
 # Cria o socket
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -56,39 +65,39 @@ while True:
     print(menu.decode('utf-8'))
     msg1 = input('digite a opção desejada: ')
     if msg1 == '1':
-        for i in range (1): #Se for 1 faça isso
+        for i in range(1):  # Se for 1 faça isso
             cliente.send(msg1.encode('utf-8'))
             msg = ' '
-            print('{:>8}'.format('%CPU')+'{:>8}'.format('%MEM'))
+            print('{:>8}'.format('%CPU') + '{:>8}'.format('%MEM'))
             # recebimento de informações
             recv = cliente.recv(1024)
             # conversão de dados
             lista = pickle.loads(recv)
-            #CPU e memória
+            # CPU e memória
             formatar_cpu_mem(lista)
             info_cpu = cliente.recv(1024)
             load = pickle.loads(info_cpu)
-            #prints do processador
-            print('Processador: ',load['brand'])
-            print('Arquitetura: ',load['arch'])
-            print('Bits: ',load['bits'])
+            # prints do processador
+            print('Processador: ', load['brand'])
+            print('Arquitetura: ', load['arch'])
+            print('Bits: ', load['bits'])
             nucleo = cliente.recv(1024)
             nucleos = pickle.loads(nucleo)
-            print('Núcleos Lógicos:',nucleos)
+            print('Núcleos Lógicos:', nucleos)
             freq = cliente.recv(1024)
             frequencia = pickle.loads(freq)
-            print('Frequência:',frequencia)
+            print('Frequência:', frequencia)
             nucleos_fisicos = cliente.recv(1024)
             nucleos = pickle.loads(nucleos_fisicos)
-            print('Núcleos Físicos:',nucleos)
+            print('Núcleos Físicos:', nucleos)
             disco_recv = cliente.recv(1024)
             disco = pickle.loads(disco_recv)
-            print(" % de Disco Usado:", disco.percent,'%')
-    elif msg1 == '2':#Se for 2 faça isso
+            print(" % de Disco Usado:", disco.percent, '%')
+    elif msg1 == '2':  # Se for 2 faça isso
         cliente.send(msg1.encode('utf-8'))
         recv = cliente.recv(2048)
         lista2 = pickle.loads(recv)
-        titulo = '{:11}'.format("Tamanho") # 10 caracteres + 1 de espaço
+        titulo = '{:11}'.format("Tamanho")  # 10 caracteres + 1 de espaço
         # Concatenar com 25 caracteres + 2 de espaços
         titulo = titulo + '{:27}'.format("Data de Modificação")
         # Concatenar com 25 caracteres + 2 de espaços
@@ -96,11 +105,11 @@ while True:
         titulo = titulo + "Nome"
         print(titulo)
         for i in lista2:
-            kb = lista2[i][0]/1000
-            tamanho = '{:10}'.format(str('{:.2f}'.format(kb)+' KB'))
+            kb = lista2[i][0] / 1000
+            tamanho = '{:10}'.format(str('{:.2f}'.format(kb) + ' KB'))
             print(tamanho, time.ctime(lista2[i][2]), " ", time.ctime(lista2[i][1]), " ", i)
         time.sleep(1)
-    elif msg1 == '3':#Se for 3 faça isso
+    elif msg1 == '3':  # Se for 3 faça isso
         cliente.send(msg1.encode('utf-8'))
         recv = cliente.recv(1024)
         dic = pickle.loads(recv)
@@ -109,21 +118,22 @@ while True:
         for i in lista:
             formatar_processos_texto(i)
         time.sleep(2)
-    elif msg1 == '4':#Se for 4 faça isso
+    elif msg1 == '4':  # Se for 4 faça isso
         cliente.send(msg1.encode('utf-8'))
         recv = cliente.recv(2048)
         dic_redes = pickle.loads(recv)
         l = ' '
         redes_formatada(l)
-        ip = dic_redes['Ethernet'][1].address
-        netmask = dic_redes['Ethernet'][1].netmask
-        mac = dic_redes['Ethernet'][0].address
-        print(ip,'      ', netmask,'              ', mac)
+        ip = dic_redes['Ethernet 4'][1].address
+        netmask = dic_redes['Ethernet 4'][1].netmask
+        mac = dic_redes['Ethernet 4'][0].address
+        print(ip, '      ', netmask, '              ', mac)
         time.sleep(2)
-    elif msg1 == '5':#Se for 5 faça isso
+    elif msg1 == '5':  # Se for 5 faça isso
         cliente.send(msg1.encode('utf-8'))
         bytes = cliente.recv(1024)
         cliente.shutdown(socket.SHUT_RDWR)
         cliente.close()
+        break
     else:
         print('Opção Inválida')
