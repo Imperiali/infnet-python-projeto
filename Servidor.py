c56dbcaadf2015cf6900e54cc383387d2a88f4f2
Lucas Barrozo <lucaslbarrozo@gmail.com>
2019-03-18 19:50:46 -0300

    cria método que aceita função e checa a subrede do ip escolhido por máquinas conectadas

--- Servidor.py
+++ Servidor.py
@@ -3,7 +3,8 @@ import os
 import pickle
 import psutil
 import socket
-
+import subprocess
+import platform
 
 # region Metodos
 
@@ -196,6 +197,37 @@ class Server:
             envio[nome] = valor
         self.envia_infos(envio)
 
+    def sub_rede(self, info):
+        def retorna_codigo_ping(hostname):
+            """Usa o utilitario ping do sistema operacional para encontrar   o host. ('-c 5') indica, em sistemas linux, que deve mandar 5   pacotes. ('-W 3') indica, em sistemas linux, que deve esperar 3   milisegundos por uma resposta. Esta funcao retorna o codigo de   resposta do ping """
+
+            plataforma = platform.system()
+            args = []
+            if plataforma == "Windows":
+                args = ["ping", "-n", "1", "-l", "1", "-w", "100", hostname]
+
+            else:
+                args = ['ping', '-c', '1', '-W', '1', hostname]
+
+            ret_cod = subprocess.call(args, stdout=open(os.devnull, 'w'), stderr=open(os.devnull, 'w'))
+            return ret_cod
+
+        def verifica_hosts(base_ip):
+            """Verifica todos os host com a base_ip entre 1 e 255 retorna uma lista com todos os host que tiveram resposta 0 (ativo)"""
+            print("Mapeando\r")
+            host_validos = []
+            return_codes = dict()
+            for i in range(1, 255):
+
+                return_codes[base_ip + '{0}'.format(i)] = retorna_codigo_ping(base_ip + '{0}'.format(i))
+                if i % 20 == 0:
+                    print(".", end="")
+                if return_codes[base_ip + '{0}'.format(i)] == 0:
+                    host_validos.append(base_ip + '{0}'.format(i))
+            print("\nMapping ready...")
+
+            return host_validos
+        return verifica_hosts(info)
     def closeConection(self):
         self.socket_server.close()
 
